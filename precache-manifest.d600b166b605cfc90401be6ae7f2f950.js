840381243ddd587215356587b4def7cfd8d6b62a
jameswquinn <jameswquinn@users.noreply.github.com>
2018-05-24 11:21:27 +0100

    initial comment

---
+++ precache-manifest.d600b166b605cfc90401be6ae7f2f950.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdf9a9e4bf2a92f061f7",
+    "url": "/js/vendors~app.fdf9a9e4.js"
+  },
+  {
+    "revision": "7b1a6762e917cffdc9c3",
+    "url": "/js/app.7b1a6762.js"
+  },
+  {
+    "revision": "3fc6c6d98d1495dabb93dfd76ab69c8c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b1a6762e917cffdc9c3",
+    "url": "/css/app.52acc41b.css"
+  }
+];
